979ba32c5b10f614c075232f87e2b19c25a07e86
yangy <1355285815@qq.com>
2024-11-19 16:31:49 +0800

    配置路由

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.scss'
+
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import App from './App.vue'
+import 'element-plus/dist/index.css'
+import router from './router'
+
+const app = createApp(App);
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
